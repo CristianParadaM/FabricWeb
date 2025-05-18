56767b0ccf458ee85440eb7c3a958a9e7f3af4d6
Cristian David Parada Martinez <crispo9028@gmail.com>
2025-05-17 21:32:56 -0500

    Init Project

---
+++ src/main/java/com/uptc/frw/fabricweb/FabricWebApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.fabricweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FabricWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FabricWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/fabricweb/FabricWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw.fabricweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FabricWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
