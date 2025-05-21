1dd3275be12dd737434c2cc0eec1e9ec59013cff
Adriana Cubides <cubides-1964@hotmail.com>
2025-05-20 23:12:30 -0500

    Feat: Create PersonService and PersonController

--- src/main/java/com/uptc/frw/fabricweb/controller/PersonController.java
+++ src/main/java/com/uptc/frw/fabricweb/controller/PersonController.java
@@ -1,4 +1,41 @@
 package com.uptc.frw.fabricweb.controller;
 
+import com.uptc.frw.fabricweb.model.Person;
+import com.uptc.frw.fabricweb.model.Product;
+import com.uptc.frw.fabricweb.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("persons")
 public class PersonController {
+    @Autowired
+    private PersonService personService;
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.findAllPersons();
+    }   
+
+    @GetMapping("/{id}")
+    public Person getPerson(@PathVariable long id) {
+        return personService.getPersonById(id);
+    }
+
+    @PostMapping
+    public Person savePerson(@RequestBody Person Person) {
+        return personService.savePerson(Person);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable long id) {
+        personService.deletePerson(id);
+    }
+
+    @PutMapping
+    public Person upDatePerson(@RequestBody Person Person) {
+        return personService.updatePerson(Person);
+    }
 }
--- src/main/java/com/uptc/frw/fabricweb/service/PersonService.java
+++ src/main/java/com/uptc/frw/fabricweb/service/PersonService.java
@@ -1,4 +1,39 @@
 package com.uptc.frw.fabricweb.service;
 
+import com.uptc.frw.fabricweb.model.Person;
+import com.uptc.frw.fabricweb.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class PersonService {
+    @Autowired
+    private PersonRepository personRepository;
+    @Autowired
+    private ProductTypeService productTypeService;
+
+    public List<Person> findAllPersons() {
+        return personRepository.findAll();
+    }
+    public Person getPersonById(Long id){
+        return personRepository.findById(id).orElse(null);
+    }
+
+    public Person savePerson (Person person){
+        return personRepository.save(person);
+    }
+    public void deletePerson(Long id){
+        personRepository.deleteById(id);
+    }
+    public  Person updatePerson(Person newPerson){
+        Person person = getPersonById(newPerson.getId());
+        person.setNames(newPerson.getNames());
+        person.setLastnames(newPerson.getLastnames());
+        person.setBirthdate(newPerson.getBirthdate());
+        person.setAddress(newPerson.getAddress());
+        person.setPhone(newPerson.getPhone());
+        person.setEmergencyContact(newPerson.getEmergencyContact());
+        return  personRepository.save(person);
+    }
 }
